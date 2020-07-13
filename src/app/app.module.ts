4f20aba1d348f6dc7e340e5a8ed1e90d2900f5ab
ThatGreenAlien <thatgreenalien@gmail.com>
2020-07-12 22:18:28 -0400

    Added mat accordian, expansion, and list to drawer sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
 	declarations: [
@@ -21,7 +23,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 		MatToolbarModule,
 		MatIconModule,
 		MatButtonModule,
-		MatSidenavModule
+		MatSidenavModule,
+		MatExpansionModule,
+		MatListModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
